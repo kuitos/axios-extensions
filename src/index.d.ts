8684615492d49c4675cffa55ee055ced65eb4735
Kuitos <kuitos.lau@gmail.com>
2018-03-20 12:55:25 +0800

    :bookmark: 2.0.0

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,9 +4,14 @@
  * @since 2018/1/12 上午11:03
  */
 import { AxiosAdapter } from 'axios';
-import * as LRU from 'lru-cache';
+import * as Cache from 'lru-cache';
 
-declare const cacheAdapterEnhancer: (adapter: AxiosAdapter, cacheEnabledByDefault?: boolean, enableCacheFlag?: string, cacheAge?: number) => AxiosAdapter;
+interface ICacheLike<K, V> {
+	get(key: K): V | undefined;
+	set(key: K, value: V, maxAge?: number): boolean;
+	del(key: K): void;
+}
+
+declare const cacheAdapterEnhancer: <K, V>(adapter: AxiosAdapter, cacheEnabledByDefault?: boolean, enableCacheFlag?: string, cacheLike?: ICacheLike<K, V>) => AxiosAdapter;
 declare const throttleAdapterEnhancer: (adapter: AxiosAdapter, threshold?: number, cacheCapacity?: number) => AxiosAdapter;
-declare const Cache: LRU;
 export { cacheAdapterEnhancer, throttleAdapterEnhancer, Cache };
