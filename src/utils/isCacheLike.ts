6ee7b2767e4864f6b9e421ccac01876b51c26515
Kuitos <kuitos.lau@gmail.com>
2022-10-02 10:32:02 +0800

    🏷 upgrade type assert (#92)

--- src/utils/isCacheLike.ts
+++ src/utils/isCacheLike.ts
@@ -3,8 +3,8 @@
  * @homepage https://github.com/kuitos/
  * @since 2018-03-19
  */
+import { ICacheLike } from './getDefaultLruCache';
 
-export default function isCacheLike(cache: any) {
-	return !!(cache.set && cache.get && cache.del &&
-		typeof cache.get === 'function' && typeof cache.set === 'function' && typeof cache.del === 'function');
+export default function isCacheLike(cache: any): cache is ICacheLike<any> {
+	return typeof cache.get === 'function' && typeof cache.set === 'function' && typeof cache.del === 'function';
 }
