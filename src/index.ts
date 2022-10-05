f14e5faebb13d5dbb5d257e4bf0ecfa2d01de828
Kuitos <kuitos.lau@gmail.com>
2022-10-05 13:12:27 +0800

    🩹 compatible with lru-cache v7

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,21 @@
  * @since 2017-09-28
  */
 
-import Cache from 'lru-cache';
+import LRUCache from 'lru-cache';
 import cacheAdapterEnhancer from './cacheAdapterEnhancer';
 import retryAdapterEnhancer from './retryAdapterEnhancer';
 import throttleAdapterEnhancer from './throttleAdapterEnhancer';
 import { ICacheLike } from './utils/getDefaultLruCache';
+
+class Cache<K, V> extends LRUCache<K, V> {
+	constructor(options: LRUCache.Options<any, any>) {
+		super(options);
+	}
+
+	del(key: K): boolean {
+		return super.delete(key);
+	}
+}
 
 export {
 	Cache,
