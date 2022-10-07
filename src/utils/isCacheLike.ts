5c36673299612ff8c8584209717fc3e27257d348
Kuitos <kuitos.lau@gmail.com>
2022-10-07 13:34:14 +0800

    🐛 compatible lru-cache v7 del method and fix its overwrote issue (#96)

--- src/utils/isCacheLike.ts
+++ src/utils/isCacheLike.ts
@@ -3,8 +3,11 @@
  * @homepage https://github.com/kuitos/
  * @since 2018-03-19
  */
-import { ICacheLike } from './getDefaultLruCache';
+export type ICacheLike<T> = {
+  get(key: string): T | undefined;
+  set(key: string, value: T): void;
+} & ({ del(key: string): void } | { delete(key: string): void });
 
 export default function isCacheLike(cache: any): cache is ICacheLike<any> {
-	return typeof cache.get === 'function' && typeof cache.set === 'function' && typeof cache.del === 'function';
+	return typeof cache.get === 'function' && typeof cache.set === 'function' && (typeof cache.delete === 'function' || typeof cache.del === 'function');
 }
